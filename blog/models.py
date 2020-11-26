b5485f2e50fc53d5d10ab9019e93e8e86de172b6
Your Name <you@example.com>
2020-11-26 18:01:25 +0000

    ok

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(unique=True)
+    field1 = models.CharField(max_length=250, default="")
+    field2 = models.CharField(max_length=250, default="")
+    field3 = models.CharField(max_length=250, default="")
+    field4 = models.CharField(max_length=250, default="")
+    field6 = models.CharField(max_length=250, default="")
